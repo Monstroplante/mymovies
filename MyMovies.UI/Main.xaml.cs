6c73f553937d8011fb867bef9183bc735d838fd8
testerez@gmail.com <testerez@gmail.com@526c527a-0aef-2e95-f767-791c1f02c510>
2011-11-01 19:43:21 +0000

    Delete missing files at end of scan to keep tags on moved movies
    git-svn-id: https://mymovies.googlecode.com/svn/trunk@72 526c527a-0aef-2e95-f767-791c1f02c510

--- MyMovies.UI/Main.xaml.cs
+++ MyMovies.UI/Main.xaml.cs
@@ -74,16 +74,7 @@ namespace MyMovies
                 dirs => {
                     scanLog.Info("Scan started!");
 
-                    //Dlete missing movies
-                    int cmpt= 0;
-                    foreach (var path in DM.Instance.GetAllFiles().Where(p => !File.Exists(p)))
-                    {
-                        DM.RemoveFile(path);
-                        cmpt++;
-                    }
-                    scanLog.Info(cmpt + " missing files deleted");
-
-                    //Search for new movies);
+                    //Search for new movies
                     foreach (String path in dirs.SelectMany(Scanner.GetFiles))
                     {
                         if(DM.Instance.HasFile(path))
@@ -120,6 +111,16 @@ namespace MyMovies
                             scanLog.Error(ex.Message);
                         }
                     }
+
+                    //Dlete missing movies
+                    int cmpt = 0;
+                    foreach (var path in DM.Instance.GetAllFiles().Where(p => !File.Exists(p)))
+                    {
+                        DM.RemoveFile(path);
+                        cmpt++;
+                    }
+                    scanLog.Info(cmpt + " missing files deleted");
+
                     return "";
                 },
                 null, null, (s, ex) => scanLog.Info("Scan finished"));
